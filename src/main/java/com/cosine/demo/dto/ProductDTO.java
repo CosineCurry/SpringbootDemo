70e227830453ea4144086ac5efcbb4b6178ad587
cosineLiFang <1597533706@qq.com>
2021-06-03 16:59:01 +0800

    优化生产者消费者模型

---
+++ src/main/java/com/cosine/demo/dto/ProductDTO.java
@@ -0,0 +1,41 @@
+package com.cosine.demo.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigInteger;
+
+/**
+ * @ClassName ProductDTO
+ * @Description 商品DTO
+ * @Author cosine
+ * @Date 2021/6/3 13:09
+ * @Version 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "商品id", required = true, example = "2021060300010001")
+    @NotNull(message = "商品id不能为空")
+    private BigInteger productId;
+
+    @ApiModelProperty(value = "商品名称", required = true, example = "哇哈哈矿泉水")
+    @NotNull(message = "商品名称不能为空")
+    private String name;
+
+    @ApiModelProperty(value = "商品类目id", required = true, example = "1000")
+    @NotNull(message = "商品类目id不能为空")
+    private Integer itemId;
+
+    @ApiModelProperty(value = "创建工厂", required = true, example = "立方")
+    @NotNull(message = "创建工厂不能为空")
+    private String createFactory;
+
+}
---
+++ src/test/java/com/cosine/demo/service/ProductServiceTest.java
@@ -0,0 +1,56 @@
+package com.cosine.demo.service;
+
+import com.cosine.demo.dto.ProductDTO;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigInteger;
+
+@SpringBootTest
+//单元测试回滚
+@Rollback
+@Transactional
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+    static final Logger logger = LoggerFactory.getLogger(ProductService.class);
+
+    /**
+     * 生产者
+     */
+    class ProducerRunnable implements Runnable {
+        @Override
+        public void run() {
+            //使用毫秒级别的时间戳生成商品id （long）
+            long productId = System.currentTimeMillis();
+            ProductDTO productDTO = new ProductDTO(new BigInteger(String.valueOf(productId)), "哇哈哈矿泉水", 1000, Thread.currentThread().getName());
+            productService.addProduct(productDTO);
+            logger.info(Thread.currentThread().getName()+"生产："+1);
+        }
+    }
+    /**
+     * 消费者
+     */
+    class ConsumerRunnable implements Runnable {
+        @Override
+        public void run() {
+            //logger.info("进入ConsumerRunnable的run方法");
+            productService.consumeProducts(1000, 1);
+            logger.info(Thread.currentThread().getName()+"消费："+1);
+        }
+    }
+
+    @Test
+    public void ProConTest() {
+        for (int i = 0; i < 30; i++) {
+            new Thread(new ProductServiceTest.ProducerRunnable()).start();
+            new Thread(new ProductServiceTest.ConsumerRunnable()).start();
+        }
+    }
+}
